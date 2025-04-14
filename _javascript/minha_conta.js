ed758afc44309964ec81a927ab125d3171228221
Ramon Valverde <ramonaguiar1998@gmail.com>
2025-04-13 22:48:24 -0300

    uploads

--- _javascript/minha_conta.js
+++ _javascript/minha_conta.js
@@ -7,21 +7,22 @@ async function logout() {
 // Checa se o usuário está logado quando a página carrega
 window.addEventListener('DOMContentLoaded', async () => {
     const { data: { user } } = await supabase.auth.getUser()
-    document.getElementById("data_user").innerText = `
-            ID: ${user.id} \n 
-            Nome: ${user.user_metadata.nome} \n
-            CPF: ${user.user_metadata.cpf} \n
-            Nascimento: ${user.user_metadata.nascimento} \n
-            Sexo: ${user.user_metadata.sexo} \n
-            Estado civil: ${user.user_metadata.estado_civil} \n
-            Celular: ${user.user_metadata.celular} \n
-            Email: ${user.email} \n
-            Estado: ${user.user_metadata.estado} \n
-            Cidade: ${user.user_metadata.cidade} \n
-            Bairro: ${user.user_metadata.bairro} \n
-            Logradouro: ${user.user_metadata.logradouro} \n
-            Número da casa: ${user.user_metadata.numero_casa} \n
-            Complemento: ${user.user_metadata.complemento} \n
-            CEP: ${user.user_metadata.cep}`;
+
+    document.getElementById("nome_usuario").innerText = `${user.user_metadata.nome}`;
+    document.getElementById("email_usuario").innerText = `${user.email}`.toLowerCase();
+
+    document.getElementById("data_nome_completo").innerText = `${user.user_metadata.nome}`.toLowerCase();
+    document.getElementById("data_cpf").innerText = `${user.user_metadata.cpf}`;
+    document.getElementById("data_celular").innerText = `${user.user_metadata.celular}`;
+    document.getElementById("data_nascimento").innerText = `${user.user_metadata.nascimento}`;
+    document.getElementById("data_civil").innerText = `${user.user_metadata.estado_civil}`.toLowerCase();
+    document.getElementById("data_sexo").innerText = `${user.user_metadata.sexo}`.toLowerCase();
+    document.getElementById("data_logradouro").innerText = `${user.user_metadata.logradouro}`.toLowerCase();
+    document.getElementById("data_cep").innerText = `${user.user_metadata.cep}`;
+    document.getElementById("data_numero_casa").innerText = `${user.user_metadata.numero_casa}`;
+    document.getElementById("data_complemento").innerText = `${user.user_metadata.complemento}`.toLowerCase();
+    document.getElementById("data_bairro").innerText = `${user.user_metadata.bairro}`.toLowerCase();
+    document.getElementById("data_cidade").innerText = `${user.user_metadata.cidade}`.toLowerCase();
+    document.getElementById("data_estado").innerText = `${user.user_metadata.estado}`;
 
 });
